0b3068bc5497e7d5ae37d39c8bdc8dbabb830995
Iara Reateran <ireateran@gmail.com>
2025-10-09 13:30:56 +0800

    view assessment(crud working1)

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,7 @@
-// backend/app.js
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
