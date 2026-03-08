f538b7354bcee27f9660d6a5ada0d101c8f7e5a5
Zein Reyes <zbcreyes@gmail.com>
2026-03-08 09:48:46 +0800

    fixed the multiplication operator.

--- backend/src/model/Assessment.js
+++ backend/src/model/Assessment.js
@@ -34,7 +34,7 @@ const assessmentSchema = new mongoose.Schema(
               type: {
                 type: String,
                 enum: [
-                  "print", "variable", "multiple", "add", "subtract", "divide",
+                  "print", "variable", "multiply", "add", "subtract", "divide",
                   "equal", "equalto", "notequal", "less", "lessequal", "greater", "greaterequal",
                   "if", "elif", "else", "while", "do-while", "for",  // ← add these three
                 ],
